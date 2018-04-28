65677c07e4e324a5a5542ca44c5555a2cfc3c388
Cristian Alejandro Trujillo Vargas <CATV@MacBook-Pro-de-Cristian.local>
2018-04-28 11:59:30 -0500

    Componentes y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {EmpresasPageComponent} from './componentes/empresas-page/empresas-page.component'
+import {EstadisticasPageComponent} from './componentes/estadisticas-page/estadisticas-page.component'
+import {LoginPageComponent} from './componentes/login-page/login-page.component'
+import {MapaPageComponent} from './componentes/mapa-page/mapa-page.component'
+import {NotFoundPageComponent} from './componentes/not-found-page/not-found-page.component'
+import {UsuariosPageComponent} from './componentes/usuarios-page/usuarios-page.component'
+
+const routes: Routes = [
+  {path:'',component:LoginPageComponent},
+  {path:'usuarios',component:UsuariosPageComponent},
+  {path:'mapa',component:MapaPageComponent},
+  {path:'empresas',component:EmpresasPageComponent},
+  {path:'estadisticas',component:EstadisticasPageComponent},
+  {path:'**',component:NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { LoginPageComponent } from './componentes/login-page/login-page.componen
 import { MapaPageComponent } from './componentes/mapa-page/mapa-page.component';
 import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
 import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
+import { EmpresasPageComponent } from './componentes/empresas-page/empresas-page.component';
+import { EstadisticasPageComponent } from './componentes/estadisticas-page/estadisticas-page.component';
+import { UsuariosPageComponent } from './componentes/usuarios-page/usuarios-page.component';
 
 
 @NgModule({
@@ -16,7 +19,10 @@ import { NotFoundPageComponent } from './componentes/not-found-page/not-found-pa
     LoginPageComponent,
     MapaPageComponent,
     NavBarComponent,
-    NotFoundPageComponent
+    NotFoundPageComponent,
+    EmpresasPageComponent,
+    EstadisticasPageComponent,
+    UsuariosPageComponent
   ],
   imports: [
     BrowserModule,
