f130d9107d1d6403ac95229852acd38f0c33dc4d
Cristian Alejandro Trujillo Vargas <CATV@MacBook-Pro-de-Cristian.local>
2018-04-28 10:40:50 -0500

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { MapaPageComponent } from './componentes/mapa-page/mapa-page.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    MapaPageComponent,
+    NavBarComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/mapa-page/mapa-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-page',
+  templateUrl: './mapa-page.component.html',
+  styleUrls: ['./mapa-page.component.scss']
+})
+export class MapaPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
